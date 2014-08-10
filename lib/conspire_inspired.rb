de96767eb2c3c6404dc543015631a84ec820d4f0
vandosant <sjskender@gmail.com>
2014-08-10 14:02:11 -0600

    Finds current and old friends

--- lib/conspire_inspired.rb
+++ lib/conspire_inspired.rb
@@ -1,12 +1,57 @@
+require 'active_support/core_ext/date'
+
 class ConspireInspired
-  attr_reader :data
+  attr_reader :emails
 
   def initialize(path)
-    @data = get_data(path)
+    @emails = get_emails(path)
 
   end
 
-  def get_data(path)
+  def relationships(email_address)
+    result = {}
+    statistics = {}
+    @emails.each do |email|
+      from_index = email.index("From: ")
+      from_email = email[(from_index)+6..-1].split("\r").first
+      to_index = email.index("To: ")
+      to_email = email[(to_index+4)..-1].split("\r").first
+      date_index = email.index("Date: ")
+      email_date_as_string = email[(date_index+6)..-1].split("\r").first
+      email_date = Date.parse(email_date_as_string)
+      if from_email == email_address
+        statistics[to_email] ||= {}
+        statistics[to_email]["sent"] ||= 0
+        statistics[to_email]["sent"] += 1
+        statistics[to_email]["last_active"] ||= email_date
+        if email_date > statistics[to_email]["last_active"]
+          statistics[to_email]["last_active"] = email_date
+        end
+      elsif to_email == email_address
+        statistics[from_email] ||= {}
+        statistics[from_email]["received"] ||= 0
+        statistics[from_email]["received"] += 1
+        statistics[from_email]["last_active"] ||= email_date
+        if email_date > statistics[from_email]["last_active"]
+          statistics[from_email]["last_active"] = email_date
+        end
+      end
+    end
+    statistics.each do |email, stats|
+      response_rate = stats["received"].to_f/stats["sent"].to_f
+      if stats["sent"] >= 3 && response_rate >= 0.66 && stats["last_active"] > Date.today.weeks_ago(2)
+        result[email] = "Current Friend"
+      elsif stats["sent"] >= 3 && response_rate >= 0.66
+        result[email] = "Old Friend"
+      end
+    end
+    result
+  end
+
+
+  private
+
+  def get_emails(path)
     result = []
     this_path = path + "/*.eml"
     Dir.glob(this_path).each do |file|
--- spec/conspire_inspired_spec.rb
+++ spec/conspire_inspired_spec.rb
@@ -5,28 +5,75 @@ require 'faker'
 require 'securerandom'
 I18n.enforce_available_locales = false
 
+CreateEmail = ->(sender, recipient, days_ago, in_reply_to = nil) {
+  id = SecureRandom.uuid
+  message = Mail.new do
+    in_reply_to in_reply_to if in_reply_to
+    to recipient
+    from sender
+    date Date.today - days_ago
+    subject Faker::Lorem.sentence
+    body Faker::Lorem.paragraphs.join("\n\n")
+    message_id "<#{id}@example.com>"
+  end
+  File.open("./spec/data/#{id}.eml", "w") { |f| f.puts message.to_s }
+  message.message_id
+}
 
 describe ConspireInspired do
   it 'parses email data from a path' do
-    CreateEmail = ->(sender, recipient, days_ago, in_reply_to = nil) {
-      id = SecureRandom.uuid
-      message = Mail.new do
-        in_reply_to in_reply_to if in_reply_to
-        to recipient
-        from sender
-        date Date.today - days_ago
-        subject Faker::Lorem.sentence
-        body Faker::Lorem.paragraphs.join("\n\n")
-        message_id "<#{id}@example.com>"
-      end
-      File.open("./spec/data/#{id}.eml", "w") { |f| f.puts message.to_s }
-      message.message_id
-    }
     CreateEmail.("user@example.com", "otheruser@example.com", 7*4)
 
     ci = ConspireInspired.new('spec/data')
 
-    expect(ci.data).to_not be_nil
+    expect(ci.emails).to_not be_nil
+  end
+
+  it 'finds current friends' do
+    MakeCurrentFriends = ->(sender, recipient) {
+      original = CreateEmail.(sender, recipient, 7*6)
+      CreateEmail.(recipient, sender, 7*5, original)
+
+      CreateEmail.(sender, recipient, 7*4)
+
+      original = CreateEmail.(sender, recipient, 7*3)
+      CreateEmail.(recipient, sender, 12, original)
+    }
+    MakeCurrentFriends.call("user@example.com", "otheruser@example.com")
+
+
+    ci = ConspireInspired.new('spec/data')
+    actual = ci.relationships("user@example.com")
+
+    expected = {"otheruser@example.com" => "Current Friend"}
+
+    expect(actual).to eq expected
+  end
+
+  it 'finds old friends' do
+    MakeOldFriends = ->(sender, recipient) {
+      [18, 20, 22, 24, 26, 28, 30, 32].each do |weeks_ago|
+        original = CreateEmail.(sender, recipient, 7*weeks_ago)
+        CreateEmail.(recipient, sender, 7*(weeks_ago - 1), original)
+      end
+
+      [15, 17].each do |weeks_ago|
+        CreateEmail.(sender, recipient, 7*weeks_ago)
+      end
+
+      [16, 18].each do |weeks_ago|
+        CreateEmail.(sender, recipient, 7*weeks_ago)
+      end
+    }
+    MakeOldFriends.call("user@example.com", "otheruser@example.com")
+
+
+    ci = ConspireInspired.new('spec/data')
+    actual = ci.relationships("user@example.com")
+
+    expected = {"otheruser@example.com" => "Old Friend"}
+
+    expect(actual).to eq expected
   end
 
   after do
